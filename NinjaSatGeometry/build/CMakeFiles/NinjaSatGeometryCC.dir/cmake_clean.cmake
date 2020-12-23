69b621eeeb4cf15ff05609c6b992d5a37a908829
xxxx <xxxxxx@gmail.com>
2020-12-23 20:58:55 +0900

    “add”

---
+++ NinjaSatGeometry/build/CMakeFiles/NinjaSatGeometryCC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NinjaSatGeometryCC.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/NinjaSatGeometryCC.dir/src/NinjaSatMainGeometry.cc.o"
+  "libNinjaSatGeometryCC.dylib"
+  "libNinjaSatGeometryCC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NinjaSatGeometryCC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
